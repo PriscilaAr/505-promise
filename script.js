2bfc45bfdb1b15e654629ccc60620ff691f04c48
Priscila Araújo <araujo16012002@gmail.com>
2019-08-02 17:48:41 -0300

    Adicionar exemplos

---
+++ script.js
@@ -0,0 +1,63 @@
+/*let diaSemana = "sextou";
+
+let callback = (resolve, reject) =>{
+    if(diaSemana == "sextou"){
+        resolve("Acertô mizeravi");
+    }
+    else{
+        reject("Errrooouu");
+    }
+}
+
+let promessa = new Promisse(callback);
+
+promessa
+    .then(function(result){
+        console.log(result);
+    })
+    .catch(function(error){
+        console.log(error);
+    })*/
+
+
+// // //
+
+
+/*let mesAno = "agosto";
+
+let callback = (resolve, reject) =>{
+    if(mesAno == "agosto"){
+        resolve("Acertô mizeravi");
+    }
+    else{
+        reject("Errrooouu");
+    }
+}
+
+let promessa = new Promise(callback);
+
+promessa
+    .then(function(result){
+        console.log(result)
+    })
+    .catch(function(errou){
+        console.log(errou);
+    })
+    .finally(console.log("Função concluida"));*/
+
+
+// // // 
+
+
+/*fetch('https://swapi.co/api/people/89')
+    .then(function(result){
+        return result.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })*/
+
+
+// // //
+
+
